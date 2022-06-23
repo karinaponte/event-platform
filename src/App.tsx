baf6b58a5b598e3f33dd357d779fad48e12075c1
karinaponte <kita_36@hotmail.com>
2022-06-23 01:06:41 -0300

    Roteamento e player da aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
-import { gql, useQuery } from "@apollo/client"
-import { Event } from "./pages/Event"
-
-
-const GET_LESSONS_QUERY = gql `
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./components/Router"
+import { client } from "./lib/apollo"
 
 function App() {
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY,
-  //   }).then(response => {
-  //     console.log(response.data)
-  //   })
-  // }, [])
-
-  // const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
-
-  // interface Lesson {
-  //   id: string;
-  //   title: string;
-  // }
-
   return (
-   <div>
-     <Event></Event>
-   </div>
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
   )
 }
 
